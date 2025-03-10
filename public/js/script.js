5200f6fdc8cd7e9db0a25ba9d716b7f21bd9bd8c
Michaël HEID <michael123456222444666@gmail.com>
2025-03-10 10:07:34 +0100

    mise en place du menu déroulant du bouton "nouveau" dans la navbar : c'est moche pour le moment mais ça fonctionne parfaitement !

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
-console.log('ok');
+/* https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("mySidenav");
+    if (x.className === "sidenav") {
+      x.className += " responsive";
+    } else {
+      x.className = "sidenav";
+    }
+  }
