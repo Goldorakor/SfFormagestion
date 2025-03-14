564a67fa40699693aa04509a132fde9fa6d7fd18
Michael Heid <michael123456222444666@gmail.com>
2025-03-14 12:23:55 +0100

    création du fichier style03.css et du fichier base03.html.twig pour préparer la mise en page et l'affichage des vues de documents administratifs

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 /* https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp */
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to sidenav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("mySidenav");
     if (x.className === "sidenav") {
