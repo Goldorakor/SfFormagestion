2de3bb6a1162000eba71dcea03d88ee3b78670af
Michael Heid <michael123456222444666@gmail.com>
2025-03-21 06:02:20 +0100

    en cours : trouver resp légal d'une société

--- src/Repository/SocieteRepository.php
+++ src/Repository/SocieteRepository.php
@@ -82,4 +82,48 @@ class SocieteRepository extends ServiceEntityRepository
     
     */
 
+
+
+    // méthode pour récupérer le prix total payé par une société donnée pour une session déterminée (prix global que paie une société pour ses salariés inscrits)
+    // utile dans le document convention mais aussi dans la vue de détails d'une session dans le suivi administratif
+    public function findRespLegal($societe_id)
+    {
+
+        $entityManager = $this->getEntityManager();
+        $query = $entityManager->createQuery("
+            SELECT s.raisonSociale, SUM(i.prix) AS totalPaye
+            FROM App\Entity\Societe s
+            JOIN s.apprenants a
+            JOIN a.inscriptions i
+            WHERE i.session = :sessionId
+            AND s.id = :societeId
+            GROUP BY s.id
+        ");
+        $query->setParameter('sessionId', $session_id);
+        $query->setParameter('societeId', $societe_id);
+
+        /* return $query->getResult();   -> [
+            ["raisonSociale" => "Nom de la société", "totalPaye" => 5000]
+        ]*/
+        return $query->getOneOrNullResult();  // -> ["raisonSociale" => "Nom de la société", "totalPaye" => 5000]
+
+    }
+
+    /* 
+
+    requête SQL liée à function findRespLegal($societe_id)
+
+    SELECT societe.raison_sociale, SUM(inscription.prix) AS total_paye
+    FROM societe
+    JOIN apprenant ON societe.id = apprenant.societe_id
+    JOIN inscription ON apprenant.id = inscription.apprenant_id
+    WHERE inscription.session_id = :sessionId
+    AND societe.id = :societeId
+    GROUP BY societe.id;
+    
+    */
+
+
+
+
 }
