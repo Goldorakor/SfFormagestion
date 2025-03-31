a0f1387f4193aa8fd00d1ec600ffd4621ad33e16
Michael Heid <michael123456222444666@gmail.com>
2025-03-31 22:00:29 +0200

    correction de l'affichage de certaines pages (mal centrées) et positionnement correct des boutons violets de bas de page

--- src/Form/RepresentantType.php
+++ src/Form/RepresentantType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RepresentantType extends AbstractType
